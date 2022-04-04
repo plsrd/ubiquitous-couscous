d53ba71ce9a2210ba981641df8763ae2393e27ba
Racheal Pennell <racheal.larimer@gmail.com>
2022-04-04 13:08:29 -0400

    finish Gatsby tutorial pt 3

--- src/pages/index.js
+++ src/pages/index.js
@@ -6,10 +6,7 @@ const IndexPage = () => {
   return (
     <Layout pageTitle='Home'>
       <p>This is the forest where I practice my witchcraft</p>
-      <StaticImage
-        alt='a very evil forest'
-        src='https://images.unsplash.com/photo-1607336652415-008df45f08a7?ixlib=rb-1.2.1&ixid=MnwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8&auto=format&fit=crop&w=774&q=80'
-      />
+      <StaticImage alt='a very evil forest' src='../images/trees.webp' />
     </Layout>
   );
 };
