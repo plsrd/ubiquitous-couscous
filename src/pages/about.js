e6536e88ac93871609aa8a6efd312d3e0a7b414b
Racheal Pennell <racheal.larimer@gmail.com>
2022-04-04 12:39:23 -0400

    finish Gastby tutorial pt 2

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Layout from '../components/Layout';
+
+const About = () => {
+  return (
+    <Layout pageTitle='About'>
+      <p>This is an about page</p>
+    </Layout>
+  );
+};
+
+export default About;
